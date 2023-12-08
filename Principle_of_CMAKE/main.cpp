b9177120fb96dc787f8ffd429f3c05b34950b9a7
Tianhao Huang <huangtianhao9@gmail.com>
2023-12-08 22:18:19 +0100

    cmake

---
+++ Principle_of_CMAKE/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "tools.hpp"
+using namespace std;
+
+int main() {
+    MakeItRain();
+    MakeItSunny();
+
+    return 0;
+}
---
+++ Principle_of_CMAKE/tools.cpp
@@ -0,0 +1,12 @@
+#include "tools.hpp"
+#include <iostream>
+
+using namespace std;
+
+void MakeItSunny{
+    cout << "It's now sunny!\n";
+}
+
+void MakeItRain{
+    cerr << "NOT yet Implemented\n";
+}
